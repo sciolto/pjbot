ff415125a6523b0509d9f641202b5fd33f462fd3
Oreste Attanasio <oreste.attanasio@wuerth-phoenix.com>
2018-07-18 19:48:28 +0200

    Create pjbot.py

---
+++ python/pjbot.py
@@ -0,0 +1,9 @@
+import requests
+# raccolgo nella variabile `testo` ciò che gli utenti scriveranno in chat al bot
+testo=Hook['params']['message']['text']
+# raccolgo la chat ID
+chatID=Hook['params']['message']['chat']['id']
+# imposto l'URL per inviare i messaggi indietro al bot
+URL='https://api.telegram.org/bot' + Hook['env']['parrot_bot_key'] + '/sendMessage'
+# invio indietro alla chat ID il messaggio ricevuto
+richiesta=requests.get(URL,verify=False,data={'chat_id':chatID,'text':testo})
