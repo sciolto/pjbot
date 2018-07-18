f9235461cea00779d26cadb8b7ea81a7b084ac25
Oreste Attanasio <oreste.attanasio@wuerth-phoenix.com>
2018-07-18 19:50:32 +0200

    added pj_bot_apikey reference

--- python/pjbot.py
+++ python/pjbot.py
@@ -4,6 +4,6 @@ testo=Hook['params']['message']['text']
 # raccolgo la chat ID
 chatID=Hook['params']['message']['chat']['id']
 # imposto l'URL per inviare i messaggi indietro al bot
-URL='https://api.telegram.org/bot' + Hook['env']['parrot_bot_key'] + '/sendMessage'
+URL='https://api.telegram.org/bot' + Hook['env']['pj_bot_apikey'] + '/sendMessage'
 # invio indietro alla chat ID il messaggio ricevuto
 richiesta=requests.get(URL,verify=False,data={'chat_id':chatID,'text':testo})
